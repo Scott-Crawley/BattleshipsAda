39ff4efa6466657f06a2575ef5036c639e8785bc
Scott-Crawley <scott.crawley@ada.ac.uk>
2021-10-11 10:30:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattleshipsAda
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
